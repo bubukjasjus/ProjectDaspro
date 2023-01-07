278d6e64fbdfdcce6af643a24e852cf20cf298d1
62821 <62821@Farhan>
2023-01-08 05:15:01 +0700

    pagiii

--- BerkahRentalKendaraan/src/main/java/com/mycompany/berkahrentalkendaraan/orderResult.java
+++ BerkahRentalKendaraan/src/main/java/com/mycompany/berkahrentalkendaraan/orderResult.java
@@ -38,13 +38,18 @@ public class orderResult extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
-        jLabel10 = new javax.swing.JLabel();
         jLabel12 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel10 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
         jLabel11 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jButton3 = new javax.swing.JButton();
+        jLabel13 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -64,23 +69,23 @@ public class orderResult extends javax.swing.JFrame {
         jLabel2.setForeground(new java.awt.Color(204, 204, 204));
         jLabel2.setText("Durasi      :");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(60, 410, 70, 30);
+        jLabel2.setBounds(60, 300, 70, 30);
 
         jLabel4.setFont(new java.awt.Font("Serif", 1, 14)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(204, 204, 204));
         jLabel4.setText("Pesanan Anda");
         jPanel1.add(jLabel4);
-        jLabel4.setBounds(60, 250, 90, 30);
+        jLabel4.setBounds(60, 220, 90, 30);
 
         jLabel5.setFont(new java.awt.Font("Serif", 0, 14)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(204, 204, 204));
         jLabel5.setText("Transmisi :");
         jPanel1.add(jLabel5);
-        jLabel5.setBounds(60, 300, 80, 30);
+        jLabel5.setBounds(60, 260, 80, 30);
 
         yourPhone.setForeground(new java.awt.Color(255, 255, 255));
         jPanel1.add(yourPhone);
-        yourPhone.setBounds(210, 190, 210, 30);
+        yourPhone.setBounds(210, 170, 210, 30);
 
         yourName.setForeground(new java.awt.Color(255, 255, 255));
         jPanel1.add(yourName);
@@ -88,7 +93,7 @@ public class orderResult extends javax.swing.JFrame {
 
         yourAddress.setForeground(new java.awt.Color(255, 255, 255));
         jPanel1.add(yourAddress);
-        yourAddress.setBounds(210, 140, 210, 30);
+        yourAddress.setBounds(210, 130, 210, 30);
 
         jLabel6.setFont(new java.awt.Font("Serif", 0, 14)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(204, 204, 204));
@@ -100,31 +105,21 @@ public class orderResult extends javax.swing.JFrame {
         jLabel7.setForeground(new java.awt.Color(204, 204, 204));
         jLabel7.setText("Alamat   :");
         jPanel1.add(jLabel7);
-        jLabel7.setBounds(60, 140, 60, 30);
+        jLabel7.setBounds(60, 130, 60, 30);
 
         jLabel3.setFont(new java.awt.Font("Serif", 0, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(204, 204, 204));
         jLabel3.setText("No Telp :");
         jPanel1.add(jLabel3);
-        jLabel3.setBounds(60, 190, 60, 30);
-
-        jLabel8.setFont(new java.awt.Font("Serif", 0, 14)); // NOI18N
-        jLabel8.setForeground(new java.awt.Color(204, 204, 204));
-        jLabel8.setText("Jumlah     :");
-        jPanel1.add(jLabel8);
-        jLabel8.setBounds(60, 350, 70, 30);
+        jLabel3.setBounds(60, 170, 60, 30);
 
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(230, 410, 180, 30);
+        jLabel1.setBounds(230, 300, 180, 30);
 
         jLabel9.setForeground(new java.awt.Color(255, 255, 255));
         jPanel1.add(jLabel9);
-        jLabel9.setBounds(230, 300, 180, 30);
-
-        jLabel10.setForeground(new java.awt.Color(255, 255, 255));
-        jPanel1.add(jLabel10);
-        jLabel10.setBounds(230, 350, 180, 30);
+        jLabel9.setBounds(230, 260, 180, 30);
 
         jLabel12.setFont(new java.awt.Font("SimSun", 0, 24)); // NOI18N
         jLabel12.setForeground(new java.awt.Color(204, 204, 204));
@@ -143,9 +138,48 @@ public class orderResult extends javax.swing.JFrame {
         jPanel1.add(jButton2);
         jButton2.setBounds(500, 380, 80, 23);
 
-        jLabel11.setIcon(new javax.swing.ImageIcon("C:\\Users\\62821\\Downloads\\Untitled design (16).png")); // NOI18N
+        jLabel8.setFont(new java.awt.Font("Serif", 0, 12)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel8.setText("TOTALNYA :");
+        jPanel1.add(jLabel8);
+        jLabel8.setBounds(50, 350, 90, 30);
+
+        jTextField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jPanel1.add(jTextField1);
+        jTextField1.setBounds(50, 380, 90, 30);
+
+        jLabel10.setFont(new java.awt.Font("Serif", 0, 12)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel10.setText("KEMBALIAN");
+        jPanel1.add(jLabel10);
+        jLabel10.setBounds(340, 350, 120, 30);
+
+        jTextField2.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jPanel1.add(jTextField2);
+        jTextField2.setBounds(350, 380, 100, 30);
+
+        jLabel11.setFont(new java.awt.Font("Serif", 0, 12)); // NOI18N
+        jLabel11.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel11.setText("Masukkan Duid Anda");
         jPanel1.add(jLabel11);
-        jLabel11.setBounds(0, 0, 600, 500);
+        jLabel11.setBounds(190, 350, 120, 30);
+
+        jTextField3.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jPanel1.add(jTextField3);
+        jTextField3.setBounds(200, 380, 100, 30);
+
+        jButton3.setBackground(new java.awt.Color(235, 168, 67));
+        jButton3.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
+        jButton3.setText("CALCULATE");
+        jPanel1.add(jButton3);
+        jButton3.setBounds(270, 430, 110, 23);
+
+        jLabel13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/mycompany/berkahrentalkendaraan/Untitled design (16).png"))); // NOI18N
+        jPanel1.add(jLabel13);
+        jLabel13.setBounds(0, 0, 600, 500);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -209,10 +243,12 @@ public class orderResult extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -222,6 +258,9 @@ public class orderResult extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
     private javax.swing.JLabel yourAddress;
     private javax.swing.JLabel yourName;
     private javax.swing.JLabel yourPhone;
